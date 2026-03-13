a81385f3fa4d8482260b00bb00731a7e2fe2caaf
Shradha0511 <shradhz2511@gmail.com>
2026-03-14 03:08:29 +0530

    add config for streamlit cloud

---
+++ config/config.py
@@ -0,0 +1,15 @@
+import os
+
+try:
+    import streamlit as st
+    GROQ_API_KEY = st.secrets.get("GROQ_API_KEY", os.environ.get("GROQ_API_KEY", ""))
+    TAVILY_API_KEY = st.secrets.get("TAVILY_API_KEY", os.environ.get("TAVILY_API_KEY", ""))
+except:
+    GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "your_groq_key_here")
+    TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY", "your_tavily_key_here")
+
+GROQ_MODEL_NAME = "llama-3.1-8b-instant"
+EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 50
+TOP_K_RESULTS = 4
